92419bef80d81c260d5d285e900dbf4f8fbb3ff4
hchauhan7816 <hchauhan7816@gmail.com>
2023-03-17 23:33:08 +0530

    added Single Statistics Files

--- frontend/src/Screens/Homepage/Homepage.js
+++ frontend/src/Screens/Homepage/Homepage.js
@@ -1,15 +1,14 @@
-import React,{useEffect,useState} from 'react'
-import Bar_Chart1 from '../../Components/Charts/Bar_Chart1'
-import Bar_Chart2 from '../../Components/Charts/Bar_Chart2'
-import Bar_Chart3 from '../../Components/Charts/Bar_Chart3'
-import Chart_Donut1 from '../../Components/Charts/Chart_Donut1'
-import Chart_Pie from '../../Components/Charts/Chart_Pie'
-import Chart_Pie1 from '../../Components/Charts/Chart_Pie1'
-import Chart_Pie2 from '../../Components/Charts/Chart_Pie2'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import Bar_Chart1 from "../../Components/Charts/Bar_Chart1";
+import Bar_Chart2 from "../../Components/Charts/Bar_Chart2";
+import Bar_Chart3 from "../../Components/Charts/Bar_Chart3";
+import Chart_Donut1 from "../../Components/Charts/Chart_Donut1";
+import Chart_Pie from "../../Components/Charts/Chart_Pie";
+import Chart_Pie1 from "../../Components/Charts/Chart_Pie1";
+import Chart_Pie2 from "../../Components/Charts/Chart_Pie2";
+import SingleStatistics from "../../Components/SingleStatistics/SingleStatistics";
 
 function Homepage() {
- 
   return (
     <>
       {/* <Chart_Pie /> */}
@@ -20,7 +19,7 @@ function Homepage() {
       {/* <Bar_Chart2 /> */}
       {/* <Bar_Chart3 /> */}
     </>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
