8cf30b49c834d2a0ddc2440deec9e346df1d614f
Himalaya Gupta <guptahimalaya2@gmail.com>
2023-03-18 00:33:48 +0530

    multi select dropdown

--- frontend/src/Screens/Homepage/Homepage.js
+++ frontend/src/Screens/Homepage/Homepage.js
@@ -7,18 +7,15 @@ import Chart_Pie from '../../Components/Charts/Chart_Pie'
 import Chart_Pie1 from '../../Components/Charts/Chart_Pie1'
 import Chart_Pie2 from '../../Components/Charts/Chart_Pie2'
 import axios from 'axios'
+import Dropdown from '../../Components/Dropdown/Dropdown'
+import Temp from '../../Components/Temp/Temp'
+import MultiSelectDropdown from '../../Components/MutliSelectDropdown/MultiSelectDropdown'
 
 function Homepage() {
- 
+
   return (
     <>
-      {/* <Chart_Pie /> */}
-      {/* <Chart_Donut1 /> */}
-      {/* <Chart_Pie1 /> */}
-      {/* <Chart_Pie2  /> */}
-      {/* <Bar_Chart1 /> */}
-      {/* <Bar_Chart2 /> */}
-      {/* <Bar_Chart3 /> */}
+    
     </>
   )
 }
